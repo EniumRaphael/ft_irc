bb90951c85818c981fcbb2ebf629f388472593cf
Raphael <rparodi@student.42.fr>
2025-05-12 14:22:40 +0200

    feat(core/main): adding debug mode to see the args

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include "core.hpp"
 
 int main(int argc, char *argv[]) {
 	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+		std::cerr << CLR_RED << "Usage: " << argv[0] << " <port> <password>" << CLR_RESET << std::endl;
 		return 1;
 	}
-	std::cout << "Port:" << argv[1] << std::endl;
-	std::cout << "Password:" << argv[2] << std::endl;
+	if (DEBUG) {
+		std::cout << CLR_GREY << "Port:\t\t" << CLR_GREEN << argv[1] << CLR_RESET << std::endl;
+		std::cout << CLR_GREY << "Password:\t" << CLR_GREEN << argv[2] << CLR_RESET << std::endl;
+	}
 	return 0;
 }

cdefa51595947a764d3ac135d2cf77dc7d979831
Raphael <rparodi@student.42.fr>
2025-05-12 14:23:30 +0200

    feat(core): adding header for simplicity

---
+++ include/core/color.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 14:13:18 by rparodi           #+#    #+#             */
+/*   Updated: 2025/05/12 14:13:30 by rparodi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#define CLR_RESET "\033[0m"
+
+#define CLR_BLACK "\033[0;30m"
+#define CLR_RED "\033[0;31m"
+#define CLR_GREEN "\033[0;32m"
+#define CLR_YELLOW "\033[0;33m"
+#define CLR_BLUE "\033[0;34m"
+#define CLR_MAGENTA "\033[0;35m"
+#define CLR_CYAN "\033[0;36m"
+#define CLR_WHITE "\033[0;37m"
+#define CLR_GOLD "\033[38;5;220m"
+#define CLR_GREY "\033[38;5;240m"

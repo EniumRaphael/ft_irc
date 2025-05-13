d4478022d02adf0e09fd6087ba436b230eb2f6f1
Raphael <rparodi@student.42.fr>
2025-05-13 12:06:09 +0200

    feat(debug): adding the log macro

--- include/core/core.hpp
+++ include/core/core.hpp
@@ -6,15 +6,22 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 14:16:03 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/13 11:06:48 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/05/13 11:59:01 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#ifdef DEBUG
+#define LOG std::endl << CLR_CYAN << "[DEBUG] " << __FILE__ << ":" << __LINE__  << std::endl << CLR_RESET
+#endif
+
 #ifndef DEBUG
 #define DEBUG 0
+#define LOG ""
 #endif
 
 #include "color.hpp"
 #include "server.hpp"
+
+unsigned short int valid_port(char *input);

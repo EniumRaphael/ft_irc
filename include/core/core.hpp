8d1fbc3c1429808ad7a8892475b19f61f1f9cf88
Raphael <rparodi@student.42.fr>
2025-05-13 12:45:48 +0200

    style(log): macro now sync with other messages

--- include/core/core.hpp
+++ include/core/core.hpp
@@ -6,14 +6,14 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 14:16:03 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/13 11:59:01 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/05/13 12:45:24 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #ifdef DEBUG
-#define LOG std::endl << CLR_CYAN << "[DEBUG] " << __FILE__ << ":" << __LINE__  << std::endl << CLR_RESET
+#define LOG std::endl << CLR_CYAN << "Debug: " << __FILE__ << ":" << __LINE__  << std::endl << CLR_RESET
 #endif
 
 #ifndef DEBUG

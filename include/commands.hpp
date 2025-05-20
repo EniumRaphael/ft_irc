1f9659613047b56c2b06b5d0734ce152e994e45b
Raphael <rparodi@student.42.fr>
2025-05-20 23:50:22 +0200

    feat(commands): channel required to send to command part

--- include/commands.hpp
+++ include/commands.hpp
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 23:31:58 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/20 23:41:28 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/05/20 23:49:46 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,29 @@
 #include <string>
 #include <vector>
 #include "user.hpp"
+#include "channel.hpp"
 
 namespace cmd
 {
-	void dispatch(User user, const std::string &line);
+	/**
+	 * @brief To send the line where a command is invoqued to execute
+	 *
+	 * @param user user who send the command
+	 * @param channel channel where the command is sent
+	 * @param line line send by the user
+	 */
+	void dispatch(User *user, Channel *channel, const std::string &line);
 	std::vector<std::string> split(const std::string &line);
 
-	class ACommand {
+	class ICommand {
 		private:
 			std::string _command;
 			std::vector<std::string> _args;
 			User _user;
 		public:
-			virtual bool execute() = 0;
-			~ACommand();
-			ACommand(User user, const std::string &line);
+			virtual void execute() = 0;
+			~ICommand();
+			ICommand(User *user, Channel *channel, const std::string &line);
 	};
 
 	class Invite;

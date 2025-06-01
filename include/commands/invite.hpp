48821409a1c387190389b6ca13deff8856343983
Raphael <rparodi@student.42.fr>
2025-06-02 00:36:36 +0200

    fix(commands/ACommand): add the forgotten constructor

--- include/commands/invite.hpp
+++ include/commands/invite.hpp
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 17:17:31 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/26 16:27:42 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/06/02 00:34:16 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 class cmd::Invite : public ACommand {
 	public:
+		Invite(User *user, Channel *channel, Server *server, const std::string &line) : ACommand(user, channel, server, line) {}
 		virtual void execute(void);
 		virtual bool checkArgs();
 };

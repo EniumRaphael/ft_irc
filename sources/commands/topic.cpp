ff5a074d81e20a822e3e8c1bbc776ac08cad59b2
Raphael <rparodi@student.42.fr>
2025-06-20 17:11:53 +0200

    fix(cmd): kick and topic in patch loading

--- sources/commands/topic.cpp
+++ sources/commands/topic.cpp
@@ -6,7 +6,7 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 17:29:48 by rparodi           #+#    #+#             */
-/*   Updated: 2025/06/18 12:51:40 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/06/20 17:09:43 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@
 using namespace cmd;
 
 e_code Topic::checkArgs() {
-	if (_args.size() < 2) {
+	if (_args.size() < 3) {
 		std::string msg461 = ":localhost 461 " + this->_sender->getNickname() + " " + this->_command + " :Not enough parameters\r\n";
 		this->_sender->appendToWriteBuffer(msg461);
 		return ERR_NEEDMOREPARAMS;
 	}
-	this->_cTarget = searchList(this->_channels, this->_args[1]);
+	if (_args.at(1).at(0) != '#') {
+		WARNING_MSG("Invalid channel name for KICK command");
+		INFO_MSG("Channel names must start with a '#' character");
+		return ERR_NOSUCHCHANNEL;
+	} else
+		_args.at(1).erase(0, 1);
+	this->_cTarget = searchList(this->_server->getChannelsList(), this->_args[1]);
 	if (this->_cTarget == NULL)
 		return ERR_NOSUCHCHANNEL;
 	if (this->_cTarget->getProtectTopic()) {
@@ -46,11 +52,15 @@ void Topic::execute() {
 		ERROR_MSG("Invalid arguments for TOPIC command (see warning message)");
 		return;
 	}
-	if (this->_cTarget->getTopic().empty()) {
-		std::string msg331 = ":localhost 331 " + this->_sender->getNickname() + " " + this->_cTarget->getName() + " :No topic is set" + "\r\n";
-		this->_sender->appendToWriteBuffer(msg331);
+	if (this->_args.size() == 1) {
+		if (this->_cTarget->getTopic().empty()) {
+			std::string msg331 = ":localhost 331 " + this->_sender->getNickname() + " " + this->_cTarget->getName() + " :No topic is set" + "\r\n";
+			this->_sender->appendToWriteBuffer(msg331);
+		} else {
+			std::string msg332 = ":localhost 332 " + this->_sender->getNickname() + " " + this->_cTarget->getName() + " :" + _cTarget->getTopic() + "\r\n";
+			this->_sender->appendToWriteBuffer(msg332);
+		}
 	} else {
-		std::string msg332 = ":localhost 332 " + this->_sender->getNickname() + " " + this->_cTarget->getName() + " :" + _cTarget->getTopic() + "\r\n";
-		this->_sender->appendToWriteBuffer(msg332);
+		this->_cTarget->setTopic(this->_args.at(2));
 	}
 }
